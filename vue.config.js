6983e2f2b69875b7ec6e4ca962365253b755b958
Benjamin Rojas <benjamin@benjaminrojas.net>
2020-04-25 15:52:08 -0400

    vue.config.js changes

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,27 @@
+const fs = require('fs')
+
+let https = false
+try {
+	/*
+	Generate Cert files via:
+	`mkcert wp-vue.local`
+	 */
+	if (fs.existsSync('./wp-vue.local-key.pem')) {
+		https = {
+			key  : fs.readFileSync('./wp-vue.local-key.pem'),
+			cert : fs.readFileSync('./wp-vue.local.pem'),
+			ca   : fs.readFileSync('/Users/benjamin/Library/Application Support/mkcert/rootCA.pem')
+		}
+	};
+} catch (err) {
+	console.log(err)
+}
+
 module.exports = {
 	productionSourceMap : false,
 	publicPath          : 'production' === process.env.NODE_ENV
 		? '/wp-content/plugins/vue-wordpress-dev-plugin/dist/'
-		: 'http://localhost:8080/',
+		: 'https://wp-vue.local:8080/',
 	outputDir        : 'dist',
 	configureWebpack : {
 		devServer : {
@@ -13,7 +32,10 @@ module.exports = {
 				'Access-Control-Allow-Methods' : '*',
 				'Access-Control-Allow-Headers' : '*'
 			},
-			disableHostCheck : true
+			disableHostCheck : true,
+			host             : 'wp-vue.local',
+			port             : '8080',
+			https
 		},
 		output : {
 			filename      : 'js/[name].js',
