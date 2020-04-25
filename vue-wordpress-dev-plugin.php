f0a0fbe6f39e75898d9616b2f4a1665522abc928
Benjamin Rojas <benjamin@benjaminrojas.net>
2020-04-25 15:22:37 -0400

    php file is complete

--- vue-wordpress-dev-plugin.php
+++ vue-wordpress-dev-plugin.php
@@ -17,41 +17,50 @@ add_action( 'admin_head', 'wpVueEnqueuePostsScripts' );
 
 function wpVueEnqueuePostsScripts() {
 	// Scripts.
-	aioseo()->helpers->enqueueScript(
-		'aioseo-posts-table',
-		'js/posts-table.js'
+	wp_enqueue_script(
+		'wp-vue-script-app',
+		$connection
+			? 'http://localhost:8080/js/app.js'
+			: plugins_url( 'dist/js/app.js', __FILE__ ),
+		[], // No dependencies.
+		'0.0.1',
+		false
 	);
-	aioseo()->helpers->enqueueScript(
-		'aioseo-posts-table-vendors',
-		'js/chunk-posts-table-vendors.js'
+	wp_enqueue_script(
+		'wp-vue-script-about',
+		$connection
+			? 'http://localhost:8080/js/about.js'
+			: plugins_url( 'dist/js/about.js', __FILE__ ),
+		[], // No dependencies.
+		'0.0.1',
+		false
 	);
-	aioseo()->helpers->enqueueScript(
-		'aioseo-common',
-		'js/chunk-common.js'
+	wp_enqueue_script(
+		'wp-vue-script-chunk-vendors',
+		$connection
+			? 'http://localhost:8080/js/chunk-vendors.js'
+			: plugins_url( 'dist/js/chunk-vendors.js', __FILE__ ),
+		[], // No dependencies.
+		'0.0.1',
+		false
 	);
 	wp_localize_script(
-		'aioseo-posts-table',
-		'aioseo',
+		'wp-vue-script-app',
+		'wpVue',
 		[
-			'restUrl'      => rest_url(),
-			'imgUrl'       => AIOSEOP_PLUGIN_IMAGES_URL, // @TODO: Maybe move this directory?
-			'posts'        => [],
-			'translations' => aioseo()->helpers->getJedLocaleData( 'all-in-one-seo-pack' ),
-			'publicPath'   => AIOSEOP_PLUGIN_URL,
-			'options'      => aioseo()->options->all(),
-			'settings'     => aioseo()->settings->all(),
-			'nonce'        => wp_create_nonce( 'wp_rest' )
+			'posts' => [],
 		]
 	);
 
 	// Styles.
-	aioseo()->helpers->enqueueStyle(
-		'aioseo-common',
-		'css/chunk-common.css'
-	);
-	aioseo()->helpers->enqueueStyle(
-		'aioseo-posts-table-style',
-		'css/posts-table.css'
+	wp_enqueue_style(
+		'wp-vue-style',
+		$connection
+			? 'http://localhost:8080/css/app.css'
+			: plugins_url( 'dist/css/app.css', __FILE__ ),
+		[], // No dependencies.
+		'0.0.1',
+		'all'
 	);
 }
 
@@ -84,26 +93,45 @@ function wpVueRenderColumn( $columnName, $postId ) {
 
 	// Add this column/post to the localized array.
 	global $wp_scripts;
-
-	$data = $wp_scripts->get_data( 'aioseo-posts-table', 'data' );
+	$data = $wp_scripts->get_data( 'wp-vue-script-app', 'data' );
 
 	if ( ! is_array( $data ) ) {
-		$data = json_decode( str_replace( 'var aioseo = ', '', substr( $data, 0, -1 ) ), true );
+		$data = json_decode( str_replace( 'var wpVue = ', '', substr( $data, 0, -1 ) ), true );
 	}
 
-	$nonce   = wp_create_nonce( "aioseo_meta_{$columnName}_{$postId}" );
-	$posts   = $data['posts'];
-	$posts[] = [
+	$nonce   = wp_create_nonce( "wp_vue_meta_{$postId}" );
+
+	$data['posts'][] = [
 		'id'         => $postId,
 		'columnName' => $columnName,
 		'nonce'      => $nonce,
 		'value'      => $value
 	];
 
-	$data['posts'] = $posts;
+	$wp_scripts->add_data( 'wp-vue-script-app', 'data', '' );
+	wp_localize_script( 'wp-vue-script-app', 'wpVue', $data );
 
-	$wp_scripts->add_data( 'aioseo-posts-table', 'data', '' );
-	wp_localize_script( 'aioseo-posts-table', 'aioseo', $data );
+	echo '<div id="<?php echo $columnName; ?>-<?php echo $postId; ?>"></div>';
+}
 
-	require( AIOSEOP_PLUGIN_DIR . 'app/Views/admin/posts/columns.php' );
+add_action( 'wp_ajax_wp_vue_ajax_save_post_meta', 'wpVueSavePostMeta' );
+
+function wpVueSavePostMeta() {
+	if ( ! current_user_can( 'edit_post', $postId ) ) {
+		wp_send_json_error();
+	}
+
+	$body   = json_decode( file_get_contents( 'php://input' ) );
+	$postId = ! empty( $body->postId ) ? intval( $body->postId ) : null;
+	$value  = ! empty( $body->value ) ? sanitize_text_field( $body->value ) : null;
+	$nonce  = ! empty( $body->_ajax_nonce ) ? $body->_ajax_nonce : null;
+
+	$result = wp_verify_nonce( $nonce, "wp_vue_meta_{$postId}" );
+	if ( empty( $result ) ) {
+		wp_send_json_error();
+	}
+
+	update_post_meta( $postId, '_wp_vue_test', $value );
+
+	wp_send_json_success();
 }
